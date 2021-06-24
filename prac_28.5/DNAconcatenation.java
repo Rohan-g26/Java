4e3d60e9c2e531548959070557965caef8f222d2
Rohan <85550870+Rohan-g26@users.noreply.github.com>
2021-06-24 14:36:22 +0530

    Add files via upload

---
+++ prac_28.5/DNAconcatenation.java
@@ -0,0 +1,15 @@
+public class DNAconcatenation 
+{
+    public static void main(String[] args) 
+   {
+
+        String dna1 = "ATTCGCTAGA";
+        String dna2 = "GCATTACC";
+
+        System.out.println("dna1 = " + dna1);
+        System.out.println("dna2 = " + dna2 + '\n');
+
+        System.out.println("dna1 + dna2 = " + (dna1 + dna2));
+
+    }
+}
